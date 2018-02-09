ff91fbe19fcc4c064acf667f6f0dfdab3d9d5f9e
Mamtha L <mamtha@apple.com>
2018-02-08 16:15:24 -0800

    Initial Commit

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,13 @@
+// var name = 'Mamtha';
+// console.log(name);
+// var lastName = "Lakshmi";
+// console.log(lastName);
+// var age = 28;
+// console.log(age);
+//
+// var fullAge = true;
+// console.log(fullAge);
+//
+// //this is boring
+// /* this
+// sucks*/
